bd61173c425d623236ed771988be48aeecdb033e
Diego Bezerra <diego.bezerra@gmail.com>
2023-11-15 11:31:18 -0300

    organizing array algorithims tests

--- src/main/kotlin/arrays_algorithms/ArraysAlgorithms.kt
+++ src/main/kotlin/arrays_algorithms/ArraysAlgorithms.kt
@@ -1,4 +1,4 @@
-package arrays
+package arrays_algorithms
 
 /**
  * Algorithms using arrays
--- src/test/kotlin/arrays/ArraysAlgorithmsTest.kt
+++ src/test/kotlin/arrays/ArraysAlgorithmsTest.kt
@@ -1,14 +1,37 @@
 package arrays
 
+import arrays_algorithms.ArraysAlgorithms
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
+@Suppress("ClassName")
 class ArraysAlgorithmsTest {
 
     private val subject = ArraysAlgorithms()
 
-    @Test
-    fun `check evens first odds last`() {
-        assertEquals(arrayOf(2, 6, 5, 9, 3).toList(), subject.evenFirstOddLast(arrayOf(2, 3, 5, 6, 9)).toList())
+    @Nested
+    inner class `check evens first odds last` {
+        @Test
+        fun `arrays with positive integers`() {
+            assertEquals(arrayOf(2, 6, 5, 9, 3).toList(), subject.evenFirstOddLast(arrayOf(2, 3, 5, 6, 9)).toList())
+            assertEquals(arrayOf(4, 6, 0, 3, 9, 7, 35).toList(), subject.evenFirstOddLast(arrayOf(35, 6, 0, 7, 3, 9, 4)).toList())
+            assertEquals(arrayOf(88, 4, 4, 0, 0, 7, 3, 3, 1).toList(), subject.evenFirstOddLast(arrayOf(1, 3, 4, 0, 0, 7, 4, 88, 3)).toList())
+        }
+
+        @Test
+        fun `arrays with negative integers`() {
+            assertEquals(arrayOf(-2, -6, -5, -9, -3).toList(), subject.evenFirstOddLast(arrayOf(-2, -3, -5, -6, -9)).toList())
+            assertEquals(arrayOf(-4, -6, 0, -3, -9, -7, -35).toList(), subject.evenFirstOddLast(arrayOf(-35, -6, 0, -7, -3, -9, -4)).toList())
+            assertEquals(arrayOf(-88, -4, -4, 0, 0, -7, -3, -3, -1).toList(), subject.evenFirstOddLast(arrayOf(-1, -3, -4, 0, 0, -7, -4, -88, -3)).toList())
+        }
+
+        @Test
+        fun `arrays with integers`() {
+            assertEquals(arrayOf(-2, 6, -5, -9, -3).toList(), subject.evenFirstOddLast(arrayOf(-2, -3, -5, 6, -9)).toList())
+            assertEquals(arrayOf(-4, -6, 0, 3, -9, -7, -35).toList(), subject.evenFirstOddLast(arrayOf(-35, -6, 0, -7, 3, -9, -4)).toList())
+            assertEquals(arrayOf(88, -4, 4, 0, 0, -7, -3, -3, -1).toList(), subject.evenFirstOddLast(arrayOf(-1, -3, 4, 0, 0, -7, -4, 88, -3)).toList())
+        }
     }
+
 }
